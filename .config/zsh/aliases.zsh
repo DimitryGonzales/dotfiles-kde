68da5a8219c93ed3b5f3076aaf289c0e28d61a9d
DimitryGonzales <125162224+DimitryGonzales@users.noreply.github.com>
2026-09-03 16:14:30 -0300

    refactor(zsh): remove git aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -9,10 +9,6 @@ alias rm="rm -v"
 alias ls="eza -lgh --icons --hyperlink --group-directories-first"
 alias lsa="eza -lagh --icons --hyperlink --group-directories-first"
 
-# Git
-alias git="$HOME/Documents/scripts/git-switch.sh && git"
-alias yadm="$HOME/Documents/scripts/git-switch.sh && yadm"
-
 # Packages
 alias clean="sudo rm -r /var/cache/pacman/pkg/download-*; yes | paru -Sccd"
 alias p="paru"
